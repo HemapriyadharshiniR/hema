f5aa853aa85e724f240c3dcbac2611a5e797cac4
HemapriyadharshiniR <hema.ramanavel1995@gmail.com>
2016-09-01 18:07:48 +0530

    Create convert.java

---
+++ convert.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class convert
+{
+public static void main(String ar[])
+{
+Scanner s=new Scanner(System.in);
+String n=s.next();
+int r=Integer.parseInt(n);
+System.out.println(r);
+}
+}

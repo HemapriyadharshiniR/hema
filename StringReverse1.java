b5760c7e60f1030f58a967dd67f286300f32c4ae
HemapriyadharshiniR <hema.ramanavel1995@gmail.com>
2016-09-01 09:36:43 -0700

    Create StringReverse1.java

---
+++ StringReverse1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class StringReverse1
+{
+public static void main(String ar[])
+{
+Scanner s=new Scanner(System.in);
+String n=s.next();
+int r=Integer.parseInt(n);
+System.out.println(r);
+}
+}

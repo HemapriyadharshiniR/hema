990834bc0af3fd2a840038c88461e48855cf5273
HemapriyadharshiniR <hema.ramanavel1995@gmail.com>
2016-09-02 12:14:21 +0530

    Create Integerarray.java

---
+++ Integerarray.java
@@ -0,0 +1,18 @@
+import jaa.io.*;
+import java.util.*;
+class Integrarray
+{
+  public static voidmain(String args[])
+  {
+   Scanners=new Scanner(System.in);
+   int n=s.nextInt();
+   int a[]=new int[n];
+   for(i=0;i<n;i++)
+     a[i]=s.nextInt();
+  String b=new String(a);
+  StringBuffer c=new StringBuffer(b);
+  c.append(0,2);
+  c.deletecharAt(0,2);
+  System.out.println(c);
+  }
+}

2faa7b02d5a034699ed222d4f9158d7db09d63aa
HemapriyadharshiniR <hema.ramanavel1995@gmail.com>
2016-09-01 17:19:11 -0700

    Create Powerofdigits2.java

---
+++ Powerofdigits2.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+class Powerofdigits2
+{
+ public static void main(String args[])
+ {
+  int i,e=1,c=0,j;
+  Scanner s=new Scanner(System.in);
+  String a=s.next();
+  
+   for(i=0;i<a.length()-1;i++)
+   {
+    int d=Character.getNumericValue(a.charAt(i));
+      System.out.println(d);
+        int b=Character.getNumericValue(a.charAt(i+1));
+       System.out.println(b);
+       for(j=0;j<b;j++)
+        {
+          e=e*d;
+        }
+     c=c+e;
+    e=1;
+   }
+    c=c+1;
+  System.out.println(c);
+ }
+}
